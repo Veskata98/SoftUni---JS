b3f2b3f3a5348bdfc8a9ecc568eaba6767b58b6b
HappyJoe777 <vescko332@gmail.com>
2022-08-09 17:34:41 +0300

    phonebook hhtp methods

--- JS Applications/Remote Data And Authentication Exercises/02.Phonebook/app.js
+++ JS Applications/Remote Data And Authentication Exercises/02.Phonebook/app.js
@@ -1,5 +1,45 @@
 function attachEvents() {
-    console.log('TODO...');
+    const url = 'http://localhost:3030/jsonstore/phonebook';
+
+    const ul = document.getElementById('phonebook');
+    const loadBtn = document.getElementById('btnLoad');
+    const createBtn = document.getElementById('btnCreate');
+
+    loadBtn.addEventListener('click', async () => {
+        ul.innerHTML = '';
+        const res = await fetch(url);
+        const data = await res.json();
+
+        Object.values(data).forEach((x) => {
+            const li = document.createElement('li');
+            li.textContent = `${x.person}: ${x.phone}`;
+
+            const delBtn = document.createElement('button');
+            delBtn.textContent = 'Delete';
+
+            li.appendChild(delBtn);
+            ul.appendChild(li);
+
+            delBtn.addEventListener('click', async () => {
+                const delResponse = await fetch(`${url}/${x._id}`, {
+                    method: 'DELETE',
+                });
+                loadBtn.click();
+            });
+        });
+    });
+    createBtn.addEventListener('click', async () => {
+        const person = document.getElementById('person').value;
+        const phone = document.getElementById('phone').value;
+
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ person, phone }),
+        });
+
+        loadBtn.click();
+    });
 }
 
 attachEvents();
